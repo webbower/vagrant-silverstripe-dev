3079dd454723815934a96cecbed5c174921b0f97
Matt Bower <matt@webbower.com>
2015-04-20 16:33:54 -0700

    First commit

---
+++ Berksfile
@@ -0,0 +1,6 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook 'silverstripe_cookbook', path: '/Users/mattbower/Sites/_vagrant/silverstripe_cookbook'
+# cookbook 'silverstripe_cookbook', github: 'webbower/silverstripe-cookbook'
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'SilverStripe Vagrant test'
+maintainer       'Matt Bower'
+maintainer_email 'matt@webbower.com'
+license          'All rights reserved'
+description      'SilverStripe Vagrant test env'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "silverstripe_cookbook"
